7b88c33dee6a80ddb639b8486e069a2cc8cf61aa
Ganzadonatien <niyocroirealine@gmail.com>
2025-01-20 18:12:19 +0200

    Encode and Decode

--- Decrypt&Encrypt.py
+++ Decrypt&Encrypt.py
@@ -1,5 +1,5 @@
-text = 'Hello Zaira!'
-custom_key = 'python'
+text = 'mrttaqrhknsw ih puggrur'
+custom_key = 'happycoding'
 
 def vigenere(message, key, direction=1):
     key_index = 0
@@ -7,8 +7,9 @@ def vigenere(message, key, direction=1):
     final_message = ''
 
     for char in message.lower():
-        # Append space to the message
-        if char == ' ':
+
+        # Append any non-letter character to the message
+        if not char.isalpha():
             final_message += char
         else:        
             # Find the right key character to encode/decode
@@ -22,8 +23,14 @@ def vigenere(message, key, direction=1):
             final_message += alphabet[new_index]
     
     return final_message
+
+def encrypt(message, key):
+    return vigenere(message, key)
     
-encryption = vigenere(text, custom_key)
-print(encryption)
-decryption = vigenere(encryption, custom_key, -1)
-print(decryption)
+def decrypt(message, key):
+    return vigenere(message, key, -1)
+
+print(f'\nEncrypted text: {text}')
+print(f'Key: {custom_key}')
+decryption = decrypt(text, custom_key)
+print(f'\nDecrypted text: {decryption}\n')
